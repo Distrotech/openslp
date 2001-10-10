a7d36178c17a97bd7dd47593fadbcd941af59466
(no author) <(no author)@3ca0f695-f50d-0410-9461-b46777378323>
2001-10-10 17:16:38 +0000

    This commit was manufactured by cvs2svn to create tag 'OpenSLP_1-0-3'.
    git-svn-id: https://openslp.svn.sourceforge.net/svnroot/openslp/tags/OpenSLP_1-0-3@1041 3ca0f695-f50d-0410-9461-b46777378323

--- openslp/libslp/libslp.h
+++ openslp/libslp/libslp.h
@@ -434,32 +434,44 @@ int KnownDAGetScopes(int* scopelistlen,
 
 
 /*=========================================================================*/
+void KnownDAProcessSrvRqst(PSLPHandleInfo handle);
+/* Process a SrvRqst for service:directory-agent                           */
+/*                                                                         */
+/* handle (IN) the handle used to make the SrvRqst                         */
+/*                                                                         */
+/* returns: none                                                           */
+/*=========================================================================*/
+
+#ifdef DEBUG
+/*=========================================================================*/
 void KnownDAFreeAll();
 /* Frees all (cached) resources associated with known DAs                  */
 /*                                                                         */
 /* returns: none                                                           */
 /*=========================================================================*/
+#endif
 
 
 
 #ifdef WIN32
 #define strncasecmp(String1, String2, Num) strnicmp(String1, String2, Num)
 #define strcasecmp(String1, String2, Num) stricmp(String1, String2, Num)
+#define inet_aton(opt,bind) ((bind)->s_addr = inet_addr(opt))
 #else
 #ifndef HAVE_STRNCASECMP
 static int
 strncasecmp(const char *s1, const char *s2, size_t len)
 {
-    while ( len-- > 1 && *s1 && (*s1 == *s2 || tolower(*s1) == tolower(*s2)) )
+    while ( *s1 && (*s1 == *s2 || tolower(*s1) == tolower(*s2)) )
     {
+        len--;
+        if(len == 0) return 0;
         s1++;
         s2++;
     }
     return(int) *(unsigned char *)s1 - (int) *(unsigned char *)s2;
 }
-
 #endif
-
 #ifndef HAVE_STRCASECMP
 static int
 strcasecmp(const char *s1, const char *s2)
@@ -472,7 +484,6 @@ strcasecmp(const char *s1, const char *s2)
     return(int) *(unsigned char *)s1 - (int) *(unsigned char *)s2;
 }
 #endif
-
 #endif /*WIN32*/
 
-#endif
+#endif /*LIBSLP_H_INCLUDED*/ 
